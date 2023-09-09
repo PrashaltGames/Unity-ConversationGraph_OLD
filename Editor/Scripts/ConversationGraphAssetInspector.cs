b6c2d9b49c7c50626954d117d20b39e158e9ea93
AtsuAtsu <58623243+AtsuAtsu0120@users.noreply.github.com>
2023-09-09 16:59:50 +0900

    feat: アッセトのインスペクターのデザインをエディターの見た目になじむように変更

--- Editor/Scripts/ConversationGraphAssetInspector.cs
+++ Editor/Scripts/ConversationGraphAssetInspector.cs
@@ -29,20 +29,30 @@ namespace Prashalt.Unity.ConversationGraph.Editor
                     var borderElement = new VisualElement()
                     {
                         style =
-                    {
+                        {
                         borderBottomColor = new Color(0.15f, 0.15f, 0.15f),
                         borderLeftColor = new Color(0.15f, 0.15f, 0.15f),
                         borderRightColor = new Color(0.15f, 0.15f, 0.15f),
                         borderTopColor = new Color(0.15f, 0.15f, 0.15f),
+
                         borderBottomWidth = 2,
                         borderLeftWidth = 2,
                         borderRightWidth = 2,
                         borderTopWidth = 2,
+
+                        borderBottomLeftRadius = 8,
+                        borderBottomRightRadius = 8,
+                        borderTopLeftRadius = 8,
+                        borderTopRightRadius = 8,
+
+                        paddingBottom = 4,
+                        paddingLeft = 4,
+                        paddingRight = 4,
+                        paddingTop = 4,
+
                         marginBottom = 5,
-                        marginLeft = 5,
-                        marginRight = 5,
                         marginTop = 5,
-                    }
+                        }
                     };
 
                     foreach (var nodeData in nodeDataList)
@@ -51,17 +61,41 @@ namespace Prashalt.Unity.ConversationGraph.Editor
                         if (type == "EndNode") continue;
                         var typeLabel = new Label($"{count}.{type}");
 
-                        typeLabel.style.fontSize = 14;
+                        typeLabel.style.fontSize = 15;
                         typeLabel.style.unityFontStyleAndWeight = FontStyle.Bold;
                         typeLabel.style.marginBottom = 3;
 
                         borderElement.Add(typeLabel);
-                        foreach (var text in JsonUtility.FromJson<ConversationData>(nodeData.json).textList)
+
+                        var followTextDescription = type switch
                         {
-                            var textElenet = new Label(text);
+                            "SelectNode" => "Options",
+                            _ => "Main Texts"
+                        };
+                        var descriptionLabel = new Label(followTextDescription);
+                        descriptionLabel.style.fontSize = 13;
+                        borderElement.Add(descriptionLabel);
+
+                        var textCount = 1;
+                        var conversation = JsonUtility.FromJson<ConversationData>(nodeData.json);
+
+                        if(conversation.speakerName != "" && conversation.speakerName is not null)
+                        {
+                            var speakerLabel = new Label($"Speaker: {conversation.speakerName}");
+                            speakerLabel.style.marginBottom = 3;
+                            speakerLabel.style.fontSize = 13;
+                            borderElement.Add(speakerLabel);
+
+                            ConversationGraphEditorUtility.MoveUp(borderElement, speakerLabel);
+                        }
+                        foreach (var text in conversation.textList)
+                        {
+                            var textElenet = new Label($"{textCount}: {text}");
                             textElenet.style.marginLeft = 10;
                             textElenet.style.marginTop = 2;
                             borderElement.Add(textElenet);
+
+                            textCount++;
                         }
                     }
                     if(borderElement.childCount != 0)
